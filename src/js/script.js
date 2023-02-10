d8866cbf018aee05b0049c5a055c45c1cacc1e94
Przemysław Nagrodzki <nagrodzkiprzemyslaw@gmail.com>
2023-02-10 14:55:32 +0100

    finish 'init accordion'

--- src/js/script.js
+++ src/js/script.js
@@ -76,24 +76,26 @@ class Product {
   }
   initAccordion(){
     const thisProduct = this
-    
-    /* find the clickable trigger (the element that should react to clicking) */
-    const clickableTrigger = ???;
 
+    /* find the clickable trigger (the element that should react to clicking) */
+    const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+    console.log(clickableTrigger)
     /* START: add event listener to clickable trigger on event click */
     clickableTrigger.addEventListener('click', function(event) {
       /* prevent default action for event */
-
+    event.preventDefault();  
       /* find active product (product that has active class) */
-
+    const activeProduct = document.querySelectorAll(select.all.menuProductsActive)  
       /* if there is active product and it's not thisProduct.element, remove class active from it */
-
+    if(activeProduct != thisProduct.element) {
+      activeProduct.classList.remove('active')
+    } 
       /* toggle active class on thisProduct.element */
+    thisProduct.element.classList.toggle('active')
     });
+  }
+  }
 
-  }
-  }
-}
 
 
 
