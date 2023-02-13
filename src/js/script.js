a847a8858f5af54ef5f6dfc75c0db27dc255bb49
Przemysław Nagrodzki <nagrodzkiprzemyslaw@gmail.com>
2023-02-13 13:12:39 +0100

    finish process order

--- src/js/script.js
+++ src/js/script.js
@@ -147,9 +147,22 @@ class Product {
       // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
       const option = param.options[optionId];
       console.log(optionId, option);
+      // check if there is param with a name of paramId in formData and if it includes optionId
+      if(formData[paramId] && formData[paramId].includes(optionId)) {
+      // Check if the option is not default //
+      if(!option.default) {
+
+      // Add option price to price variable //
+        price += option.price
+      }
+      // Check if the the option is default //
+      else if(option.default){
+      // reduce price variable //
+       price -= option.price  
+      }
     }
   }
-
+  }
   // update calculated price in the HTML
   thisProduct.priceElem.innerHTML = price;
 
