b3227f385fce14269f3a5cf21aaea50586a86edf
Przemysław Nagrodzki <nagrodzkiprzemyslaw@gmail.com>
2023-02-15 13:54:17 +0100

    Creating 'GetElements

--- src/js/script.js
+++ src/js/script.js
@@ -199,6 +199,16 @@ class Product {
 
       console.log('AmoutWidget:', thisWidget)
       console.log('constructor argments:', element)
+
+      thisWidget.getElements(element);
+    }
+    getElements(element){
+      const thisWidget = this;
+    
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
     }
   }
 
