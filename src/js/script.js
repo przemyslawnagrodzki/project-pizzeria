c6ac9316ba04fe75b6834670e6dc4e7ce033f16e
Przemysław Nagrodzki <nagrodzkiprzemyslaw@gmail.com>
2023-02-15 14:19:09 +0100

    Create 'Set value'

--- src/js/script.js
+++ src/js/script.js
@@ -201,6 +201,7 @@ class Product {
       console.log('constructor argments:', element)
 
       thisWidget.getElements(element);
+      thisWidget.setValue(thisWidget.input.value)
     }
     getElements(element){
       const thisWidget = this;
@@ -209,6 +210,20 @@ class Product {
       thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
       thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
       thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    }
+    setValue(value){
+      const thisWidget = this
+
+      const newValue = parseInt(value)
+
+      /* TODO Add validation */
+
+      thisWidget.value = newValue
+      thisWidget.input.value = thisWidget.value
+
+      if(thisWidget.value !== newValue && !isNaN(newValue)){ 
+        thisWidget.value = newValue
+      }
     }
   }
 
