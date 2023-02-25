3d8dce2eb854dd0d062da070e5d1e565292f8747
Przemysław Nagrodzki <nagrodzkiprzemyslaw@gmail.com>
2023-02-25 17:19:49 +0100

    divide js ito components

---
+++ src/js/app.js.js
@@ -0,0 +1,66 @@
+import {settings, select, classNames} from './settings.js'
+import Product from './components/product.js'
+import Cart from './components/Cart.js'
+import CartProduct from './components/CartProduct.js'
+import AmountWidget from './components/AmountWidget.js'
+import utils from './utils.js'
+
+  const app = {
+    initMenu: function(){
+      const thisApp = this;
+      // console.log('thisApp.data:', thisApp.data);
+      
+      for(let productData in thisApp.data.products){
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+      }
+    },
+    
+    initData: function(){
+      const thisApp = this;
+      thisApp.data = {};
+      const url = settings.db.url + '/' + settings.db.products;
+    
+
+      fetch(url)
+        .then(function(rawResponse){
+          return rawResponse.json();
+        })
+        .then(function(parsedResponse){
+          console.log('parsedResponse', parsedResponse);
+
+          /* save parsedResponse as thisApp.data.products */
+          thisApp.data.products = parsedResponse;
+          /* execute initMenu method */
+          thisApp.initMenu();
+        });
+
+      console.log('thisApp.data', JSON.stringify(thisApp.data));
+    },
+
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+
+      thisApp.productList = document.querySelector(select.containerOf.menu)
+
+      thisApp.productList.addEventLstener('add-to-cart', function(){
+        app.cart.add(event.detail.Product);
+      })
+    },
+
+    init: function(){
+      const thisApp = this;
+      // console.log('*** App starting ***');
+      //  console.log('thisApp:', thisApp);
+      // console.log('classNames:', classNames);
+      //console.log('settings:', settings);
+      //console.log('templates:', templates);
+      
+      thisApp.initData();
+      thisApp.initCart();
+    },
+  };
+
+  app.init();
