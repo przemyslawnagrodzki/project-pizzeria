3f9f3fec92775c742f35b33e6083aaaaa38f9712
Przemysław Nagrodzki <nagrodzkiprzemyslaw@gmail.com>
2023-02-26 18:35:00 +0100

    Add podstrony

--- src/js/app.js.js
+++ src/js/app.js.js
@@ -2,7 +2,56 @@ import {settings, select, classNames} from './settings.js'
 import Product from './components/product.js'
 import Cart from './components/Cart.js'
 
-  const app = {
+  const app = {   
+    initPages: function(){
+      const thisApp = this
+
+      thisApp.pages = document.querySelector(select.containerOf.pages).children
+      thisApp.navLinks = document.querySelector(select.nav.links)
+
+      const idFromHash = window.location.hash.replace('#/', '')
+     // console.log('idFromHash', idFromHash)
+
+      let pageMatchinHash = thisApp.pages[0].id
+
+      for( let page of thisApp.pages){
+        if(page.id == idFromHash)
+        pageMatchinHash = page.id
+        break
+      }
+
+      thisApp.activatePage(pageMatchinHash)
+
+      for( let link of thisApp.navLinks){
+        link.addEventLstener('click', function(event){
+          const clickedElement = this
+          event.preventDefault()
+
+          /* get page Id from href */
+          const id = clickedElement.getAttribute('href').replace('#', '')
+
+          /* run thisApp activatePage with that id */
+          thisApp.activatePage(id)
+
+          /* change url hash */
+          window.location.hash = '#/' + id
+        })
+      }
+    },
+    activatePage: function(pageId){
+      const thisApp = this
+
+      /* add class 'active' to matching pages, remove from non-matching */
+      for( let page of thisApp.pages){        
+          page.classList.toggle(classNames.pages.active, page.id == pageId)           
+      }
+
+      /* add class 'active' to matching links, remove from non-matching */
+      for( let link of thisApp.navLinks){       
+          link.classList.toggle(classNames.nav.active, link.getAttribute('href') == '#' + pageId)            
+      }
+    },
+
     initMenu: function(){
       const thisApp = this;
       // console.log('thisApp.data:', thisApp.data);
@@ -57,6 +106,7 @@ import Cart from './components/Cart.js'
       
       thisApp.initData();
       thisApp.initCart();
+      thisApp.initPages()
     },
 
     
